de5ad7d9007c5aece0d810be447c0d7c8a44f246
Matheeuus <matheuspadilha2012@hotmail.com>
2023-11-07 11:55:27 -0300

    Pam API Rest

---
+++ src/components/bannerFilmes/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Image, Text } from "react-native";
+import style from "./style";
+
+export default function BannerFilmes(){
+        
+    return(
+
+       <>
+            <Text style = {style.textoBanner}>Bem-Vindo</Text>
+       </> 
+
+    );
+}
---
+++ src/screens/Apagar/style.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        paddingHorizontal: 20,
+      },
+      label: {
+        fontSize: 16,
+        marginBottom: 5,
+      },
+      input: {
+        borderWidth: 1,
+        borderColor: 'black',
+        borderRadius: 10,
+        padding: 10,
+        marginBottom: 20,
+
+      },
+      button:{
+        backgroundColor:'#b48fe7',
+      }
+});
+
+
+export default style
